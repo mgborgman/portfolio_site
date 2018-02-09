e63c576cda39e6b1de06142e99b6fa4597fe154d
Matt Borgman <mgborgman@gmail.com>
2018-02-09 07:01:43 -0800

    added html file and renamed php file

---
+++ php/form.php
@@ -0,0 +1,31 @@
+<html>
+    <body>
+        <h1>This should output</h1>
+        <?php
+            // $range = [];
+            // function create_range($number) {
+            //     foreach(range(1, $number, 1) as $item) {
+            //         $range[] = $item;
+            //     }
+            //     return $range;
+            // }
+
+            // function is_prime($possible_prime) {
+            //     foreach (range(2, $possible_prime - 1, 1) as $divisor) {
+            //         if ($possible_prime % $divisor == 0) {
+            //             return FALSE;
+            //         } else {
+            //             continue;
+            //         }
+            //     }
+            //     return TRUE;
+            // }
+            // $result = is_prime(5);
+            // var_dump($result);
+
+            $num1 = "5";
+            $num2 = "5";
+            echo $num1 + $num2;
+        ?>
+    </body>
+</html>
